9a7b909999bbb2afacb3d645d3217833d06ee4e2
Madhuri Upadhye <mupadhye@redhat.com>
2024-12-13 16:54:18 +0530

    Tests: Minor fix in permission of snippet files
    Update the permission of snippet files to rw-r-----.
    
    Signed-off-by: Madhuri Upadhye <mupadhye@redhat.com>

--- src/tests/multihost/alltests/test_config_merging.py
+++ src/tests/multihost/alltests/test_config_merging.py
@@ -33,6 +33,8 @@ class TestConfigMerge(object):
         content = "[%s]\nuse_fully_quailified_name = False" % section
         snippet_file = "/etc/sssd/conf.d/01_snippet.conf"
         multihost.client[0].put_file_contents(snippet_file, content)
+        chmod_sni = f"chmod o-r {snippet_file}"
+        multihost.client[0].run_command(chmod_sni, raiseonerr=False)
         returncode = multihost.client[0].service_sssd('start')
         if returncode == 0:
             config_check = 'sssctl config-check'
@@ -63,6 +65,8 @@ class TestConfigMerge(object):
         file_content = "[%s]\nuse_fully_quailified_name = False" % dom_section
         snippet_file = "/etc/sssd/conf.d/._01_snippet.conf"
         multihost.client[0].put_file_contents(snippet_file, file_content)
+        chmod_sni = f"chmod o-r {snippet_file}"
+        multihost.client[0].run_command(chmod_sni, raiseonerr=False)
         start = multihost.client[0].service_sssd('start')
         sssctl_cmd = 'sssctl config-check'
         if start == 0:
@@ -113,7 +117,7 @@ class TestConfigMerge(object):
         file_content = "[%s]\nuse_fully_qualified_names = False" % dom_section
         snippet_file = "/etc/sssd/conf.d/01_snippet.conf.disable"
         multihost.client[0].put_file_contents(snippet_file, file_content)
-        cmd_chmod = 'chmod 600 %s' % snippet_file
+        cmd_chmod = 'chmod 640 %s' % snippet_file
         multihost.client[0].run_command(cmd_chmod, raiseonerr=False)
         start = multihost.client[0].service_sssd('start')
         if start == 0:
@@ -143,7 +147,7 @@ class TestConfigMerge(object):
         group = (''.join(choice(ascii_uppercase) for _ in range(10)))
         user_group = '{}:{}'.format(user, group)
         multihost.client[0].put_file_contents(snippet_file, file_content)
-        cmd_chmod = 'chmod 600 %s' % snippet_file
+        cmd_chmod = 'chmod 640 %s' % snippet_file
         multihost.client[0].run_command(cmd_chmod, raiseonerr=False)
         useradd = 'useradd %s' % user
         groupadd = 'groupadd %s' % group
